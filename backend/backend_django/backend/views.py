dd3270447e5ced2c5501ed5cb07f5159f14f5ced
Joel Erreyes <erreyesjoel.gms@gmail.com>
2025-09-28 00:23:42 +0200

    template de ejemplo eliminado

--- backend/backend_django/backend/views.py
+++ backend/backend_django/backend/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render # importante para renderizar plantillas como html
 
-def ejemplo_template(request):
-    return render(request, 'template_principal_ejemplo.html')
-
 # def para renderizar el template de login
 def login_template(request):
     return render(request, 'autenticacion/login.html')
