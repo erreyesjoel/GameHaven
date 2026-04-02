723cd3a717edfd4c7efd3fb2bab50b9f08c2423a
Joel Erreyes <erreyesjoel.gms@gmail.com>
2026-04-02 23:31:10 +0200

    editar usuario

--- backend/backend_django/usuarios/urls.py
+++ backend/backend_django/usuarios/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_usuarios, name='listar_usuarios'),
+    path('<int:usuario_id>/editar', views.editar_usuario, name='editar_usuario'),
 ]
--- backend/backend_django/usuarios/views.py
+++ backend/backend_django/usuarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from usuarios.models import ModeloUsuarioModificado
 from django.contrib.auth.decorators import login_required
 
@@ -8,3 +8,21 @@ from django.contrib.auth.decorators import login_required
 def listar_usuarios(request):
     lista_usuarios = ModeloUsuarioModificado.objects.all()
     return render(request, 'admin/usuarios.html', {'lista_usuarios': lista_usuarios, 'active_page': 'usuarios'})
+
+@login_required
+def editar_usuario(request, usuario_id):
+    usuario = get_object_or_404(ModeloUsuarioModificado, id=usuario_id)
+    if request.method == 'POST':
+        usuario.username = request.POST.get('username', usuario.username)
+        usuario.email = request.POST.get('email', usuario.email)
+        usuario.rol = request.POST.get('rol', usuario.rol)
+        usuario.first_name = request.POST.get('first_name', usuario.first_name)
+        usuario.last_name = request.POST.get('last_name', usuario.last_name)
+        usuario.save()
+        return redirect('listar_usuarios')
+    
+    return render(request, 'admin/editarUsuario.html', {
+        'usuario': usuario,
+        'roles': ModeloUsuarioModificado.Roles.choices,
+        'active_page': 'usuarios'
+    })
